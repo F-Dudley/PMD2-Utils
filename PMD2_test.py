0ea120da7d11192b4a96055e817936cd1ac586bc
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2025-05-20 16:53:21 +0100

    Changed Test Script

--- PMD2_test.py
+++ PMD2_test.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
 
     test_period = 10  # seconds
     test_samples = 100
+    poll_interval = 8  # ms (Match the poll interval set in PMD2)
 
-    with PowerMonitor() as pm:
+    with PowerMonitor(n_samples=test_samples, poll_interval=poll_interval) as pm:
         time.sleep(test_period)
